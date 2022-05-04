f6abb22f2bc1780eafdc49db99e071d546d8c184
Misael Flores <63472060+misadevflores@users.noreply.github.com>
2022-05-04 15:27:24 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+
+const upload = multer();
+const app = express();
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append('AMP-Access-Control-Allow-Source-Origin', process.env.URL);
+  res.append('Access-Control-Expose-Headers', ['AMP-Access-Control-Allow-Source-Origin']);
+  next();
+});
+
+// This serves static files from the specified directory
+app.use(express.static(__dirname + '/public'));
+
+app.get('/productos/filter', (req, res) => {
+  const filterType = req.query.category || 'all';
+  const sortValue = req.query.sort || 'price-desc';
+  const itemId = req.query.itemId || 'all';
+  const products = fs.readFileSync(__dirname + '/json/products.json');
+  const productsJSON = JSON.parse(products);
+  const filteredItems = productsJSON.items.filter(item => filterType == 'all' ? true : item.type == filterType);
+  const sortedFilteredItems = filteredItems.sort((a, b) => sortValue == 'price-asc' ? a.price - b.price : b.price - a.price);
+  let filteredProducts = { "items": sortedFilteredItems }
+  if (itemId != 'all') {
+    filteredProducts = { "items": productsJSON.items.filter(item => item.id == itemId) };
+  };
+  res.send(JSON.stringify(filteredProducts));
+});
+
+app.post('/submit-form', upload.array(), (req, res) => {
+  res.append('Content-Type', 'application/json');
+  res.send(JSON.stringify(req.body));
+});
+
+
+
+const server = app.listen(8081, '127.0.0.1', () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
