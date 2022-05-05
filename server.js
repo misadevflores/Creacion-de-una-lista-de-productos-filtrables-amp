76973585231869e2118b887e6b1d70737e9b5e00
Misael Flores <63472060+misadevflores@users.noreply.github.com>
2022-05-05 13:18:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use((req, res, next) => {
 // This serves static files from the specified directory
 app.use(express.static(__dirname + '/public'));
 
-app.get('/productos/filter', (req, res) => {
+app.get('/products/filter', (req, res) => {
   const filterType = req.query.category || 'all';
   const sortValue = req.query.sort || 'price-desc';
   const itemId = req.query.itemId || 'all';
-  const products = fs.readFileSync(__dirname + '/json/products.json');
+  const products = fs.readFileSync(__dirname + '/public/json/products.json');
   const productsJSON = JSON.parse(products);
   const filteredItems = productsJSON.items.filter(item => filterType == 'all' ? true : item.type == filterType);
   const sortedFilteredItems = filteredItems.sort((a, b) => sortValue == 'price-asc' ? a.price - b.price : b.price - a.price);
@@ -39,9 +39,9 @@ app.post('/submit-form', upload.array(), (req, res) => {
 
 
 
-const server = app.listen(8081, 'https://misadevflores.github.io/Creacion-de-una-lista-de-productos-filtrables-amp/', () => {
+const server = app.listen(8081, '127.0.0.1', () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('App listening at https://misadevflores.github.io/Creacion-de-una-lista-de-productos-filtrables-amp/', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
